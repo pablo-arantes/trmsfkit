d8d7cf9942b34090043db4150612dbd7a1497582
Pablo Ricardo Arantes <35934150+pablo-arantes@users.noreply.github.com>
2024-06-25 16:55:55 -0700

    Update __init__.py

--- tRMSF/__init__.py
+++ tRMSF/__init__.py
@@ -4,6 +4,8 @@ A package to perform time-dependent RMSF analysis on molecular dynamics data.
 """
 
 # Add imports here
+from .trmsfkit import trmsfkit
+
 from importlib.metadata import version
 
 __version__ = version("trmsfkit")
